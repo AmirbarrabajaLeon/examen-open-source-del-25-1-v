20b4e20b4056d48d1827e113354d4a56e324d34b
AmirbarrabajaLeon <amircasan@gmail.com>
2025-12-14 21:38:53 -0500

    fea: Update CreateOrbitThresholdCommand to include orbitClass and maxSafeDuration fields

--- src/main/java/com/spire/platform/u202310680/regulations/domain/model/commands/CreateOrbitThresholdCommand.java
+++ src/main/java/com/spire/platform/u202310680/regulations/domain/model/commands/CreateOrbitThresholdCommand.java
@@ -1,4 +1,4 @@
 package com.spire.platform.u202310680.regulations.domain.model.commands;
 
-public record CreateOrbitThresholdCommand() {
+public record CreateOrbitThresholdCommand(String orbitClass, Integer maxSafeDuration) {
 }
