51cfb8e0ceca411da7fe9387f0e6935f27859e38
AmirbarrabajaLeon <amircasan@gmail.com>
2025-12-14 20:35:38 -0500

    fea: Add MissionAssignmentRepository for managing mission assignments

---
+++ src/main/java/com/spire/platform/u202310680/missions/infrastructure/persistence/jpa/repositories/MissionAssignmentRepository.java
@@ -0,0 +1,13 @@
+package com.spire.platform.u202310680.missions.infrastructure.persistence.jpa.repositories;
+
+import com.spire.platform.u202310680.missions.domain.model.aggregates.MissionAssignment;
+import com.spire.platform.u202310680.missions.domain.model.valueobjects.SatelliteCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+@Repository
+public interface MissionAssignmentRepository extends JpaRepository<MissionAssignment, Long> {
+    boolean existsBySatelliteCodeAndRequestedAtBetween(SatelliteCode satelliteCode, LocalDateTime start, LocalDateTime end);
+}
